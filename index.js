3b3f7fc0af48f8b7fcd86f524d6d8c96e0ae0157
Kelly <kelly@truenorth.us>
2023-03-14 13:52:25 -0400

    looping lap lol

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+let occasion = [];
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        occasion.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`, ); 
+    }
+    return occasion;
+}
+
+function countDown(x){
+    let y = parseInt(x);
+    while (y >= 0) {
+        console.log(y);
+        y--;
+    }
+}
